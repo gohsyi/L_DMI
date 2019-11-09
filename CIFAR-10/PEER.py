88345ea193791db6bd42bb6251872f7422aa8467
henry <guohongyi@sjtu.edu.cn>
2019-11-09 22:52:23 +0800

    add peer loss

--- CIFAR-10/PEER.py
+++ CIFAR-10/PEER.py
@@ -1,4 +1,4 @@
-# CE - training with cross entropy loss
+# peer - training with L_peer
 
 import torch.nn.parallel
 import torch.optim
@@ -8,7 +8,6 @@ from model import *
 from dataset import *
 
 num_classes = 10
-
 CE = nn.CrossEntropyLoss().cuda()
 
 
@@ -40,7 +39,7 @@ def test(model, test_loader=test_loader_):
     correct = 0
     total = 0
 
-    for i, (idx, input, target) in enumerate(tqdm(test_loader)):
+    for i, (idx, input, target) in enumerate(test_loader):
         input = torch.Tensor(input).cuda()
         target = torch.autograd.Variable(target).cuda()
 
@@ -54,27 +53,74 @@ def test(model, test_loader=test_loader_):
     return accuracy
 
 
+def validate(valid_loader, model, criterion):
+
+    model.eval()
+    loss_t = 0
+
+    with torch.no_grad():
+
+        for i, (idx, input, target) in enumerate(valid_loader):
+            if i == 1:
+                break
+
+            input = torch.autograd.Variable(input.cuda())
+            target = torch.autograd.Variable(target.cuda())
+
+            output = model(input)
+            loss = criterion(output, target)
+            loss_t += loss
+
+    #print('valid_loss=', loss_t.item())
+    return loss_t
+
+
+def validate_acc(valid_loader, model, criterion):
+
+    model.eval()
+    loss_t = 0
+    correct = 0.0
+    total = 0.0
+    with torch.no_grad():
+
+        for i, (idx, input, target) in enumerate(valid_loader):
+            if i == 1:
+                break
+            total += target.size(0)
+            input = torch.autograd.Variable(input.cuda())
+            target = torch.autograd.Variable(target.cuda())
+
+            output = model(input)
+            _, predicted = output.max(1)
+            correct += predicted.eq(target).sum().item()
+
+        accuracy = 100. * correct / total
+
+    #print('valid_loss=', loss_t.item())
+    return accuracy
+
+
 def main_peer(alpha):
     model_peer = torch.load('./model_ce_' + str(args.r) + '_' + str(args.s))
-    best_valid_loss = test(test_loader=valid_loader_noisy, model=model_peer, criterion=CE)
+    best_valid_loss = validate_acc(valid_loader=valid_loader_noisy, model=model_peer, criterion=CE)
     torch.save(model_peer, './model_peer_' + str(args.r) + '_' + str(args.s))
     test_acc = test(model=model_peer, test_loader=test_loader_)
     print('test_acc=', test_acc)
-
     for epoch in range(100):
         print("epoch=", epoch,'r=', args.r)
-
         learning_rate = 1e-6
+
         optimizer_peer = torch.optim.SGD(model_peer.parameters(), momentum=0.9, weight_decay=1e-4, lr=learning_rate)
-        #print("traning model_ce...")
-        train(train_loader=train_loader_noisy, peer_data_loader=train_loader_noisy_peer_data, alpha=alpha,
-              peer_labels_loader=train_loader_noisy_peer_labels, model=model_ce, optimizer=optimizer_ce)
-        #print("validating model_ce...")
-        valid_acc = test(model=model_peer, test_loader=valid_loader_noisy)
-        print('valid_acc=', valid_acc)
-        if valid_acc >= best_peer_acc:
-            best_peer_acc = valid_acc
-            torch.save(model_peer, './model_peer_' + str(args.r) + '_' + str(args.s) + str(alpha))
+
+        train(train_loader=train_loader_noisy, model=model_peer, peer_data_loader=train_loader_noisy_peer_data,
+              peer_labels_loader=train_loader_noisy_peer_labels, optimizer=optimizer_peer, criterion=CE, alpha=alpha)
+
+        valid_loss = validate_acc(valid_loader=valid_loader_noisy, model=model_peer, criterion=CE)
+        test_acc = test(model=model_peer, test_loader=test_loader_)
+        print('test_acc=', test_acc)
+        if valid_loss > best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model_peer, './model_peer_' + str(args.r) + '_' + str(args.s) + '_' + str(alpha))
             print("saved.")
 
 
@@ -85,8 +131,7 @@ def evaluate(path):
 
 
 if __name__ == '__main__':
-    for alpha in [1.0, -5, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 2.0, 3.0, 4.0, 5.0]:
+    print("peer:")
+    for alpha in [-5, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0]:
         main_peer(alpha)
-        print("PEER traning finished")
         evaluate('./model_peer_' + str(args.r) + '_' + str(args.s) + '_' + str(alpha))
-
